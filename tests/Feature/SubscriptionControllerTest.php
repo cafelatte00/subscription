ceeb46b8583bc6b0b3cf2a2df8d2b80ce370c5a8
cafelatte00 <latte0012345@gmail.com>
2025-03-04 15:22:38 +0900

    delete sample test

--- tests/Feature/SubscriptionControllerTest.php
+++ tests/Feature/SubscriptionControllerTest.php
@@ -163,16 +163,4 @@ class SubscriptionControllerTest extends TestCase
             'id' => $subscription->id,
         ]);
     }
-
-    /**
-     * A basic feature test example.
-     *
-     * @return void
-     */
-    public function test_example()
-    {
-        $response = $this->get('/');
-
-        $response->assertStatus(200);
-    }
 }
